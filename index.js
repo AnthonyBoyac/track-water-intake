985fa25bdbc54eee0de1c13d92179b415b3435ab
AnthonyBoyac <anthony.f.boyac@gmail.com>
2022-07-20 16:39:03 -0700

    It works, but needs styling

--- index.js
+++ index.js
@@ -1,21 +1,85 @@
-// if (localStorage.getItem("dailyIntake") === null) {
-//   localStorage.setItem("dailyIntake", "0")
-// } else {
-//   document.getElementById("track_water_intake").innerHTML = localStorage.getItem("dailyIntake")
-// }
+// localStorage.clear();
 
-function playAudio() {
+var presets = document.getElementById("presets")
+var presetOptions = document.querySelector(".preset-options")
+if (localStorage.getItem("dailyIntake") === null) {
+  localStorage.setItem("dailyIntake", "0")
+}
+document.getElementById("track_water_intake").innerHTML = localStorage.getItem("dailyIntake")
+for (var i = 0; i < localStorage.length; i++) {
+  if (localStorage.key(i) !== "dailyIntake") {
+    var option = document.createElement("option")
+    option.innerHTML = `
+      <option value="${localStorage.getItem(localStorage.key(i))}">
+        ${localStorage.key(i)}: ${localStorage.getItem(localStorage.key(i))}
+      </option>
+    `
+    presets.appendChild(option)
+  }
+}
+console.log(presets.length)
+
+if (presets.length > 0) {
+  presetOptions.style.visibility = "visible"
+  OnSelectChange()
+}
+
+// current daily intake: 3/5
+
+// play audio when certain condition are met
+function PlayAudio() {
   console.log(document.getElementById("test-audio"))
   document.getElementById("test-audio").play()
 }
 
-function updateWater() {
+function WaterRecommendationMen() {
+  document.querySelector(".water-recommendation-women").style.display = "none"
+  document.querySelector(".water-recommendation-men").style.display = "block"
+}
+function WaterRecommendationWomen() {
+  document.querySelector(".water-recommendation-men").style.display = "none"
+  document.querySelector(".water-recommendation-women").style.display = "block"
+}
+
+function OnSelectChange() {
+  let selectedValue = presets.options[presets.selectedIndex].value
+  selectedValue = selectedValue.substring(selectedValue.indexOf(" ") + 1, selectedValue.length)
+  document.getElementById("input_water_intake").value = selectedValue
+}
+
+// add preset to the list and localStorage
+function AddPreset() {
+  if (presetOptions.classList.contains("hidden")) {
+    presetOptions.style.visibility = "visible"
+  }
+  var presetName = document.getElementById("preset-name")
+  var presetSize = document.getElementById("preset-size")
+  if (presetName.value === "" || presetSize.value === "") {
+    alert("Please fill out both inputs")
+    throw new Error("You need to fill out both inputs")
+  } else {
+    var option = document.createElement("option")
+    option.innerHTML = `
+      <option value="${presetName.value}">
+        ${presetName.value}: ${presetSize.value}
+      </option
+    `
+    localStorage.setItem(presetName.value, presetSize.value)
+    presets.appendChild(option)
+    presetName.value = ""
+    presetSize.value = ""
+  }
+
+}
+
+// update current water total based on user's input
+function UpdateWater() {
   var waterInput = (parseInt(document.getElementById("input_water_intake").value))
   if (waterInput <= 0 || isNaN(waterInput)) {
     alert("Number needs to be greater than zero")
     throw new Error("Invalid number. Expected greater than zero");
   } else {
-    playAudio()
+    PlayAudio()
     var trackWater = document.getElementById("track_water_intake")
     var totalWaterInput = (parseInt(trackWater.innerHTML))
     var total = waterInput + totalWaterInput
